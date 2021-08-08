861c00d934fa564cc1d9ecc0932e13e903c51081
Farheen khan <farheenfirdous12101999@gmail.com>
2021-08-08 03:10:54 -0700

    hamburger menu animation
    Added cross X type animation to little hamburger menu

--- Easybank landing page/app/js/script.js
+++ Easybank landing page/app/js/script.js
@@ -1,1 +1,11 @@
-console.log('hey');
+const btnHamburger = document.querySelector("#btnHamburger");
+
+btnHamburger.addEventListener('click' , function(){
+    console.log('hey');
+    if(btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open');
+    }
+    else {
+        btnHamburger.classList.add('open')
+    }
+})
