f604a642b446111f9526159b37fe7b3160ea6a15
Farheen khan <farheenfirdous12101999@gmail.com>
2021-08-12 08:55:45 -0700

    Mobile Menu Done

--- Easybank landing page/app/js/script.js
+++ Easybank landing page/app/js/script.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body');
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector('.header');
 const overlay = document.querySelector(".overlay");
@@ -7,13 +8,19 @@ const fadeElems = document.querySelectorAll('.has-fade');
 btnHamburger.addEventListener('click' , function(){
     console.log('hey');
     if(header.classList.contains('open')) {// hHamburger Menu Closed
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach(function(element){
+            element.classList.remove('fade-in');
+           element.classList.add('fade-out');
+        })
+        
     }
     else {
         //Hamburger Menu opens
+        body.classList.add('noscroll');
        header.classList.add('open');
+      
        fadeElems.forEach(function(element) {
         element.classList.remove('fade-out');
         element.classList.add('fade-in');
